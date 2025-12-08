6b4d6019911f57061f683edf7d7d8e745c38e0d0
juliangf94 <juliangf94@hotmail.com>
2025-12-08 13:13:28 +0100

    Add the 3-binary_tree_delete.c  file

---
+++ holbertonschool-binary_trees/3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: a pointer to the root node of the tree to delete
+ *
+ * This function performs a post-order traversal (Left -> Right -> Root)
+ * to ensure that all child nodes are freed before the parent node.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (NULL);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
