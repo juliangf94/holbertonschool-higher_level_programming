1d27fe0981c48733dd55a70bda897f01bb5456e5
cousinfabien <cousin.fabien3@gmail.com>
2025-12-08 09:50:38 +0000

    Add : Task 0

---
+++ holbertonschool-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+* binary_tree_node - creates a new node
+* @parent: pointer to the parent node
+* @value: integer value stocked in the node
+* Return: pointer to the new node if success, NULL if failure
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
+}
