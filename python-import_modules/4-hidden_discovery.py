f27b5f0f4accbbda1b96d265e4d03dbdbf55f88d
juliangf94 <juliangf94@hotmail.com>
2026-01-15 00:42:09 +0100

    Fix task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 
-
-def discovery():
-    names = dir(hidden_4)
-    for name in sorted(names):
+if __name__ == "__main__":
+    for name in sorted(dir(hidden_4)):
         if name[:2] != "__":
             print("{}".format(name))
-
-
-if __name__ == "__main__":
-    discovery()
