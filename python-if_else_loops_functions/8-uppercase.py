196227f85152fc72ad2bd46331a15c4cba666cb3
juliangf94 <juliangf94@hotmail.com>
2026-01-14 21:36:27 +0100

    Fix the 0-positive_or_negative.py  8-uppercase.py files

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print("{:s}".format(char), end="")
+    print("")
