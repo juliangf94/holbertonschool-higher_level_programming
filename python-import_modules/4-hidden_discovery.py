176976bca12005b24b201ea1c56eb0aace046da5
Julian GONZALEZ <juliangf94@hotmail.com>
2026-01-16 14:17:29 +0000

    Task 4: Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 
+
 if __name__ == "__main__":
     for name in sorted(dir(hidden_4)):
-        if name[:2] != "__":
+        if not name.startswith("__"):
             print("{}".format(name))
