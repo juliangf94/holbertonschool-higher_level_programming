b1d9cebc4b105bc36c5a3fa07ae6e24137b61e14
juliangf94 <juliangf94@hotmail.com>
2026-03-05 17:54:44 +0100

    Fix the task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,26 +9,23 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    # Capture arguments from command line
-    db_user = sys.argv[1]
-    db_password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-    # Establish connection
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=db_user,
-        passwd=db_password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
     # Create cursor
     cursor = db.cursor()
     # Use parameterized query (%s) to prevent SQL injection.
-    # We pass the query string and a tuple containing the user input separately.
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    # We pass the query string and a tuple containing
+    # the user input separately.
     # Execute the query passing the state_name as a tuple element
-    cursor.execute(query, (state_name,))
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+        (sys.argv[4],)
+    )
     # Fetch and print results
     query_rows = cursor.fetchall()
     for row in query_rows:
