7ad4109f3d2e24ee7606d4cfe69e077de3a45162
juliangf94 <juliangf94@hotmail.com>
2026-03-05 17:50:10 +0100

    Fix the task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument passed.
-"""
+"""Displays all states matching the name argument from hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
@@ -21,7 +18,10 @@ if __name__ == "__main__":
     # Construct the SQL query using format() as requested
     # BINARY ensures the match is case-sensitive (e.g., 'Arizona' != 'arizona')
     # Execute the formatted query
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' "
+        "ORDER BY id ASC".format(sys.argv[4])
+    )
     # Fetch and print the results
     query_rows = cursor.fetchall()
     for row in query_rows:
