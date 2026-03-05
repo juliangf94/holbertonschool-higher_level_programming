1f6485a39a944f98a9efb0275d7d9ea47c57c895
juliangf94 <juliangf94@hotmail.com>
2026-03-05 14:27:30 +0100

    Fix task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,27 +8,21 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    # Capture the 4 arguments from the command line
-    db_user = sys.argv[1]
-    db_password = sys.argv[2]
-    db_name = sys.argv[3]
     state_name = sys.argv[4]
-
     # Establish connection to the database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=db_user,
-        passwd=db_password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
     # Create cursor object
     cursor = db.cursor()
     # Construct the SQL query using format() as requested
     # BINARY ensures the match is case-sensitive (e.g., 'Arizona' != 'arizona')
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(state_name)
     # Execute the formatted query
-    cursor.execute(query)
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(state_name))
     # Fetch and print the results
     query_rows = cursor.fetchall()
     for row in query_rows:
