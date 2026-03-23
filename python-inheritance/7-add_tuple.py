a45d285cf7690daf6583fecc1f3410db1dd22a68
juliangf94 <juliangf94@hotmail.com>
2026-03-23 13:42:17 +0100

    Add python inheritance main files

---
+++ python-inheritance/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    Adds the first two elements of two tuples
+    """
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+
+    new_tuple = (a[0] + b[0], a[1] + b[1])
+
+    return new_tuple
+
